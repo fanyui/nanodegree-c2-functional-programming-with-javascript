bb25d97204c577685b6b2e612cc4c869de7d0d2b
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-01-23 12:55:10 +0100

    Added image slider and reformated it look only for desktop view

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -3,6 +3,7 @@ let store = {
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
+let slideIndex = 1;
 
 // add our markup to the page
 const root = document.getElementById('root')
@@ -20,32 +21,18 @@ const render = async (root, state) => {
 // create content
 const App = (state) => {
     let { rovers, apod, roverData } = state
-    console.log("state rovers is ", state)
 
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
             ${SelectRovers(rovers)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+                <section class="slider-container">
                 ${displayRoversAndImageSlicer(roverData)}
-            </section>
+                </section>
         </main>
         <footer></footer>
     `
 }
-
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
     render(root, store)
@@ -53,28 +40,41 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+// this function takes in an array of images and returns the specified data expected as well as the slider for displaying theimages
 const displayRoversAndImageSlicer = (roverData) => {
     if(!roverData){
-        return
+        return "";
     }
     else{
-        return roverData.photos.map(photo => photo.img_src) 
+        
+        let pht = roverData.photos.map((photo, key, array) => `<div class="mySlides">
+        <div class="numbertext"> ${key+1} / ${array.length}</div>
+        <img class="slider-image" src=${photo.img_src} >
+        </div>`)
+        
+        let thumbnail = roverData.photos.map((elt, id) => `<div class="column">
+        <img class="demo cursor " src=${elt.img_src} style="width:100%" onclick="currentSlide(${id+1})" alt=${elt.camera.full_name}>
+        </div>`
+        )
+        
+        return `<div id="innital-render" class="container"> ${pht.join('')}  
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+        <div class="caption-container">
+        <p id="caption"></p>
+        </div>
+        <div class="row">
+        ${thumbnail.join('')}
+        </div>
 
+        </div>`
+        
     }
 }
-// display selection box for rovers
+
+
+// display selection box for rovers to choose the options of rover they want to see.
 
 const SelectRovers = (rovers) => {
     let roverSelect = rovers.map(rover => `<option value=${rover} > ${rover} </option>`)
@@ -85,52 +85,8 @@ const SelectRovers = (rovers) => {
 function myFunction() {
     let x = document.getElementById("mySelect").value;
     getRoverDetails(x)
-    alert(x)
-}
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        console.log("looking for apod")
-        getImageOfTheDay(store)
-    }
-    console.log( "image of the day type ", apod)
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
 }
 
-// ------------------------------------------------------  API CALLS
-
-// Example API call
-const getImageOfTheDay = async (state) => {
-    let { apod } = state
-
-    let data = await fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => {
-            console.log( apod)
-            updateStore(store, { apod })
-    })
-
-    return data
-}
 
 const getRoverDetails = (rover) => {
     let body = { rover: rover }
@@ -153,4 +109,32 @@ const getRoverDetails = (rover) => {
         })
 
     return data
+}
+//showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
