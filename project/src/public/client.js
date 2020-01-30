7e4297a794433d672f168013ea631cc3842a3302
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-01-30 12:51:53 +0100

    Seperate teh slider javascript code from client.js file and also link immutable using the cdn link

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,12 +1,17 @@
 // import { List } from 'immutable'
 // const { List } = require('immutable');
+/*
+I resulted to using the browser immutable js
+since i wasn't able to import it directly from nodemodules in this file.
+as such I chosed to use it as in
+Immutable.List 
+*/
 
 let store = {
     user: { name: "Student" },
     apod: '',
     rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
 }
-let slideIndex = 1;
 
 // add our markup to the page
 const root = document.getElementById('root')
@@ -93,7 +98,7 @@ const displayRoversAndImageSlicer = (roverData) => {
 const SelectRovers = (rovers) => {
     let roverSelect = rovers.map(rover => `<option value=${rover} > ${rover} </option>`)
     return `
-        <select id="mySelect" onchange="myFunction()">${roverSelect}</select>
+        <select id="mySelect" class="select-field" onchange="myFunction()">${roverSelect}</select>
     `
 }
 function myFunction() {
@@ -120,39 +125,16 @@ const getRoverDetails = (rover) => {
         .then(res => res.json())
         .then(roversdata => {
             const {rovers} = store
-            let rover = roversdata.rovers.photos[0].rover.name
+        /*find it index in the state. reason is we need to update the ui
+        to reflext the currently bding displayed rovers information in the select
+        box and because the render function runs and things refresh. we have to reorder
+        them here.
+        */
+            let rover = roversdata.rovers.photos[0].rover.name //get the name of the rover
            let index = rovers.indexOf(rover)
            let newRovers = rovers.splice(index,1)
             updateStore(store, { rovers: newRovers.unshift(rover), roverData: roversdata.rovers })
         })
 
     return data
-}
-//showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
